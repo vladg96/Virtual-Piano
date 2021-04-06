d76a35b56a0148fdedc1319f6c4613f956fccd77
VladG <60361148+vladg96@users.noreply.github.com>
2021-04-06 20:42:00 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,43 @@
+let notes = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'W', 'E', 'T', 'Y', 'U'];
+tiles = document.querySelectorAll('kbd');
+
+function play_note(index, color_change) {
+    let sound = new Audio('jskeys/' + notes[index] + '.mp3');
+    sound.play();
+    if (color_change) {
+        if (index < 7) {
+            tiles[index].style.backgroundColor = "#f0d4ca";
+        }
+        else {
+            tiles[index].style.backgroundColor = "#5a5858";
+        }
+    }
+}
+
+document.addEventListener("keydown", function (event){
+    notes.forEach(function (item, index) {
+        if (event.code === 'Key' + item){
+            play_note(index, true);
+        }
+    });
+});
+
+document.addEventListener("keyup", function (event) {
+    notes.forEach(function (item, index) {
+        if (event.code === 'Key' + item){
+            if (index < 7) {
+                tiles[index].style.backgroundColor = "white"
+            }
+            else {
+                tiles[index].style.backgroundColor = "black"
+            }
+        }
+    });
+});
+
+
+tiles.forEach(function (tile, index) {
+    tile.addEventListener("click", function () {
+        play_note(index, false);
+    });
+});
